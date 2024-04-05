6f8578a8ee428a3090a5fc47516e002805ef3464
Duc Nguyen <dnguyen416@gatech.edu>
2024-04-05 11:10:35 -0400

    feat: Infrastructure Layer Setup (Clean Architecture)

---
+++ CleanArchitecture.Infrastructure/Data/BlogDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> dbContextOptions) : base(dbContextOptions) { }
+        public DbSet<Blog> Blogs { get; set; }
+    }
+}
