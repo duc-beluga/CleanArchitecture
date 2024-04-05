5cb9c81b8945c4c1769d989bab450cdee2e99869
Duc Nguyen <dnguyen416@gatech.edu>
2024-04-05 12:45:05 -0400

    feat: API Layer Setup (Clean Architecture)

--- CleanArchitecture.API/Program.cs
+++ CleanArchitecture.API/Program.cs
@@ -1,3 +1,9 @@
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Interface;
+using CleanArchitecture.Infrastructure.Data;
+using CleanArchitecture.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<BlogDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BlogDbConnectionString") ?? 
+    throw new InvalidOperationException("'BlogDbConnectionString' not found"))
+);
+
+builder.Services.AddTransient<IBlogRepository, BlogRepository>();
+builder.Services.AddTransient<IBlogService, BlogService>();
 
 var app = builder.Build();
 
