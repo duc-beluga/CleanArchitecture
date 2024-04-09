dcc38acc9033521d846deae588713b8b2d4b8ec4
Duc Nguyen <dnguyen416@gatech.edu>
2024-04-09 13:38:49 -0400

    feat: enable Swagger for production

--- CleanArchitecture.API/Program.cs
+++ CleanArchitecture.API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
