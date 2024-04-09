376ec1bc66de4f42b529da17a24e05c5af33cc98
Duc Nguyen <dnguyen416@gatech.edu>
2024-04-09 02:21:12 -0400

    refractor: remove Id from CreatePost request body

--- CleanArchitecture.Application/Blog/Commands/CreateBlog/CreateBlogCommand.cs
+++ CleanArchitecture.Application/Blog/Commands/CreateBlog/CreateBlogCommand.cs
@@ -11,7 +11,6 @@ namespace CleanArchitecture.Application.Blog.Commands.CreateBlog
 {
     public class CreateBlogCommand : IRequest<CreateBlogResponse>
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Author { get; set; }
